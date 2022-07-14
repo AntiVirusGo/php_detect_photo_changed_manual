3aa11c79a4d213a377b644a46a26720a515df744
shenyanjian <shenyanjian@blingsec.cn>
2022-07-14 22:51:02 +0800

    新增回溯匹配算法和图形化算法

--- similar_pic_algorithms/pipei.py
+++ similar_pic_algorithms/pipei.py
@@ -10,6 +10,8 @@ import re
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+from loguru_log import log_data, log_debug
+from matrix_gui.matrix_gui import DiGraph
 
 def aHash(img):
     # 均值哈希算法
@@ -190,23 +192,23 @@ def runAllImageSimilaryFun(para1, para2):
         img1 = cv2.imread(para1)
         img2 = cv2.imread(para2)
 
-    hash1 = aHash(img1)
-    hash2 = aHash(img2)
-    n1 = cmpHash(hash1, hash2)
-    # print('均值哈希算法相似度aHash：', n1)
-    single_dict['aHash'] = n1
-
-    hash1 = dHash(img1)
-    hash2 = dHash(img2)
-    n2 = cmpHash(hash1, hash2)
-    # print('差值哈希算法相似度dHash：', n2)
-    single_dict['dHash'] = n2
-
-    hash1 = pHash(img1)
-    hash2 = pHash(img2)
-    n3 = cmpHash(hash1, hash2)
-    # print('感知哈希算法相似度pHash：', n3)
-    single_dict['pHash'] = n3
+    # hash1 = aHash(img1)
+    # hash2 = aHash(img2)
+    # n1 = cmpHash(hash1, hash2)
+    # # print('均值哈希算法相似度aHash：', n1)
+    # single_dict['aHash'] = n1
+    #
+    # hash1 = dHash(img1)
+    # hash2 = dHash(img2)
+    # n2 = cmpHash(hash1, hash2)
+    # # print('差值哈希算法相似度dHash：', n2)
+    # single_dict['dHash'] = n2
+    #
+    # hash1 = pHash(img1)
+    # hash2 = pHash(img2)
+    # n3 = cmpHash(hash1, hash2)
+    # # print('感知哈希算法相似度pHash：', n3)
+    # single_dict['pHash'] = n3
 
     n4 = classify_hist_with_split(img1, img2)
 
@@ -214,32 +216,39 @@ def runAllImageSimilaryFun(para1, para2):
     # print(type(n4))
     single_dict['SanZhiFang'] = n4
 
-    n5 = calculate(img1, img2)
-    # print("单通道的直方图", n5)
-    single_dict['DanTongDao'] = n5
-
-
-    # print("%d %d %d %.2f %.2f " % (n1, n2, n3, round(n4[0], 2), n5[0]))
-    # print("%.2f %.2f %.2f %.2f %.2f " % (1 - float(n1 / 64), 1 -
-    #                                      float(n2 / 64), 1 - float(n3 / 64), round(n4[0], 2), n5[0]))
-
-    plt.subplot(121)
-    plt.imshow(Image.fromarray(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)))
-    plt.subplot(122)
-    plt.imshow(Image.fromarray(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)))
-    # plt.show()
+    # n5 = calculate(img1, img2)
+    # # print("单通道的直方图", n5)
+    # single_dict['DanTongDao'] = n5
+    #
+    #
+    # # print("%d %d %d %.2f %.2f " % (n1, n2, n3, round(n4[0], 2), n5[0]))
+    # # print("%.2f %.2f %.2f %.2f %.2f " % (1 - float(n1 / 64), 1 -
+    # #                                      float(n2 / 64), 1 - float(n3 / 64), round(n4[0], 2), n5[0]))
+    #
+    # plt.subplot(121)
+    # plt.imshow(Image.fromarray(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)))
+    # plt.subplot(122)
+    # plt.imshow(Image.fromarray(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)))
+    # # plt.show()
 
     return single_dict
 
-def get_max_from_res_img_similar( res_img_similar):
-    # 传入的 res_img_similar 是一个列表，其元素是一个字典
-    # Eg: [ {'filename':['文件名1', '文件名2'], 'SanZhiFang': 0.5, 'DanTongDao':2, ...} ]
+def get_max_from_res_img_similar(res_img_similar):
+    # 传入的 res_img_similar 是一个列表，其元素是一个经过 'filename'字段中的两个文件比较数据 的字典
+    # 返回值为 文件名， 如果返回None说明没有可比文件。
+    # Eg: [
+    #       {'filename': ['input_Cam000.png', 'input_Cam001.png'], 'aHash': 1, 'dHash': 4, 'pHash': 0, 'SanZhiFang': array([0.8800225], dtype=float32), 'DanTongDao': array([0.8678064], dtype=float32)},
+    #       {'filename': ['input_Cam000.png', 'input_Cam002.png'], 'aHash': 2, 'dHash': 6, 'pHash': 1, 'SanZhiFang': array([0.8866753], dtype=float32), 'DanTongDao': array([0.8766418], dtype=float32)},
+    #   ]
+
+    # 变量 max 和 max_item 是本函数中用于最大值比较的中间变量
     max = 0
     max_item = None
+
     for single_res_img_similar in res_img_similar:
         # print(single_res_img_similar['filename'][1], single_res_img_similar['SanZhiFang'][0])
-        # 如果要更改三直方图数值算法为加权算法
-        # 可以改为 0.4* single_res_img_similar['SanZhiFang'] + 0.6* single_res_img_similar['pHash']
+        # 如果要更改最相似匹配算法，可以在这里更改。比如，可以改为 0.4* single_res_img_similar['SanZhiFang'] + 0.6* single_res_img_similar['pHash']
+        # 目前两张图最相似算法：三直方图数值算法为加权算法
         if single_res_img_similar['SanZhiFang'][0] >= max:
             max_item = single_res_img_similar['filename'][1]
             max =  single_res_img_similar['SanZhiFang'][0]
@@ -247,65 +256,128 @@ def get_max_from_res_img_similar( res_img_similar):
         return None
     return max_item
 
+def get_max_from_res_img_similar_back_one(res_img_similar):
+    # 传入的 res_img_similar 是一个列表，其元素是包含两个dict的元组
+    # 返回值为 文件名， 如果返回None说明没有可比文件。
+    # Eg: [
+    #       (
+    #               {'filename':
+    #                       ['input_Cam040.png', 'input_Cam000.png'],
+    #                'SanZhiFang':
+    #                       array([0.87966466], dtype=float32)
+    #                },
+
+    # 变量 max 和 max_item 是本函数中用于最大值比较的中间变量
+    max = 0
+    max_item = None
+
+    for single_res_img_similar in res_img_similar:
+        # print(single_res_img_similar['filename'][1], single_res_img_similar['SanZhiFang'][0])
+        # 如果要更改最相似匹配算法，可以在这里更改。比如，可以改为 0.4* single_res_img_similar['SanZhiFang'] + 0.6* single_res_img_similar['pHash']
+        # 目前两张图最相似算法：三直方图数值算法为加权算法
+        target_score = single_res_img_similar[0]['SanZhiFang'][0] + single_res_img_similar[1]['SanZhiFang'][0]
+        if target_score >= max:
+            max_item = single_res_img_similar[0]['filename'][1]
+            max =  target_score
+    if max_item == None:
+        return None
+    return max_item
+
+def SingleImgSimilar_Order(img_init, img_list):
+    """单图寻找最像 代码中最重要的部分，用于进行排序算法"""
+
+    # [!] 程序结果：相似度排序列表 similar_order
+    similar_order = []
+    similar_order.append(img_init)
+
+    # 下一个代码段要进行排序，使用 img_list2 来进行处理
+    # 对于文件名列表，这里需要深拷贝赋值给列表 img_list_cycle
+    img_list2 = img_list.copy()
+    # 因为入口img已经确认，待测试图片中可删去
+    img_list2.remove(img_init)
+
+    target_img = os.path.join(img_dir, img_init)
+
+    for i in img_list:
+        res_img_similar = []  # res_img_similar 用于临时存储本轮中 剩余图片 与 target_img 的相似比较结果
+        for img_list_index in range(len(img_list2)):
+            res_img_similar.append(runAllImageSimilaryFun(target_img, os.path.join(img_dir, img_list2[img_list_index])))
+        most_similar_img_filename = get_max_from_res_img_similar(res_img_similar)
+        img_list2.remove(most_similar_img_filename)
+        if img_list2 == []:
+            break
+        similar_order.append(most_similar_img_filename)
+        print(similar_order)
+        target_img = os.path.join(img_dir, most_similar_img_filename)
+    log_data.success("排序结果为："+str(similar_order))
+    return similar_order, "单图寻找最像"
+
+
+def BackOneImgSimilar_Order(img_init, img_list):
+    """回溯1图寻找 代码中最重要的部分，用于进行排序算法"""
+
+    # [!] 程序结果：相似度排序列表 similar_order
+    similar_order = []
+    similar_order.append(img_init)
+
+    # 下一个代码段要进行排序，使用 img_list2 来进行处理
+    # 对于文件名列表，这里需要深拷贝赋值给列表 img_list_cycle
+    img_list2 = img_list.copy()
+    # 因为入口img已经确认，待测试图片中可删去
+    img_list2.remove(img_init)
+
+    global img_dir
+    target_img = os.path.join(img_dir, img_init)
+
+    for i in img_list:
+        res_img_similar = []  # res_img_similar 用于临时存储本轮中 剩余图片 与 target_img 的相似比较结果
+
+        if len(similar_order)>=2:
+            target_img_back_one = os.path.join(img_dir, similar_order[-2])
+        else:
+            target_img_back_one = os.path.join(img_dir, similar_order[-1])
+
+        log_data.debug(" 正在测试： " + str(target_img) + "回溯: " + target_img_back_one )
+        for img_list_index in range(len(img_list2)):
+            # 从剩余图片拿出来要与 target 图片进行比较的图片 是  img_list2[img_list_index]
+
+            # 本节图片比较
+            target_compare_score = runAllImageSimilaryFun(target_img, os.path.join(img_dir, img_list2[img_list_index]))
+            # 回溯一张图片比较
+
+            back_target_score = runAllImageSimilaryFun(target_img_back_one, os.path.join(img_dir, img_list2[img_list_index]))
+            # 添加元组 (本节图片比较, 回溯一张图片比较) 至 res_img_similar
+            res_img_similar.append((target_compare_score, back_target_score))
+
+        most_similar_img_filename = get_max_from_res_img_similar_back_one(res_img_similar)
+        img_list2.remove(most_similar_img_filename)
+        if img_list2 == []:
+            break
+        similar_order.append(most_similar_img_filename)
+        print(similar_order)
+        target_img = os.path.join(img_dir, most_similar_img_filename)
+    log_data.success("排序结果为："+str(similar_order))
+    return similar_order, "回溯1图寻找"
+
+
 if __name__ == "__main__":
-    # p1 = "E:/pycharm project/bicycle/input_Cam000.png"
-    # p2 = "E:/pycharm project/bicycle/input_Cam001.png"
-    # img_dir 是目录字符串
-    img_dir = r"""E:\pycharm project\bicycle"""
-    # os.listdir 返回一个列表，该目录下所有文件的文件名
-    img_list = os.listdir(img_dir)[:15]
-    img_list_cycle = img_list.copy()
 
-    for img_list_cycle_index in range(len(img_list)):
+    # [CONFIG]
+    # 数据源图片所在目录
+    global img_dir
+    img_dir = r"""./bicycle/"""
+    # 图片入口。
+    img_init = "input_Cam040.png"
+    log_debug.info("图片入口: {IMG_INIT}".format(IMG_INIT=img_init))
 
-        img_list_cycle = img_list.copy()
+    # [DEBUG]
+    # start 如果用于测试，这里可以只匹配15张图片：os.listdir(img_dir)[:15]
+    img_list = os.listdir(img_dir)
+    log_debug.info("测试图片数量：{IMG_LIST_LEN}".format(IMG_LIST_LEN=str(len(img_list))))
+    # end
 
-        single_target_img_filename = img_list[img_list_cycle_index]
-        print("TARGET: " + single_target_img_filename)
-        # 路径名拼接
-        raw_img = os.path.join(img_dir, single_target_img_filename)
-        img_list_cycle.remove(single_target_img_filename)
-
-
-
-        global similar_order
-        similar_order = []
-        similar_order.append(single_target_img_filename)
-
-        img_list_cycle_num = len(img_list_cycle)
-
-        # for range
-        for cycle_num in range( img_list_cycle_num):
-            res_img_similar = []
-            for single_img in  img_list_cycle:
-                # 这个for循环是每个图片和目标图片进行比较，然后结果放入res_img_similar列表
-                res_img_similar.append( runAllImageSimilaryFun(raw_img, os.path.join(img_dir, single_img)))
-            most_similar_img_filename = get_max_from_res_img_similar(res_img_similar)
-            similar_order.append(most_similar_img_filename)
-            raw_img = os.path.join(img_dir, most_similar_img_filename)
-            img_list_cycle.remove(most_similar_img_filename)
-
-            # print("第{CYCLE_NUM}轮：{SIMILAR_ORDER}".format(CYCLE_NUM=cycle_num+1, SIMILAR_ORDER=similar_order))
-
-        # 按照order写入文件
-        target_dir = os.path.join(r"E:\pycharm project", single_target_img_filename)
-        os.mkdir(target_dir)
-        for similar_order_index in range(len(similar_order)):
-            with open(os.path.join(img_dir, similar_order[similar_order_index]), 'rb') as f:
-                img_content = f.read()
-                f.close()
-            with open(os.path.join(target_dir, "name_"+str(similar_order_index)+'.png'), 'wb') as f:
-                f.write(img_content)
-                f.close()
-
-        for similar_order_index in range(len(similar_order)):
-            filename_num = re.search(r"input_Cam(\d{3}).png", similar_order[similar_order_index]).group(1)
-            similar_order[similar_order_index] = filename_num
-        print(str(similar_order))
-
-
-
-
-
+    # DiGraph(ImgOrder(img_dir, img_init, img_list))
+    # DiGraph()
+    DiGraph(BackOneImgSimilar_Order(img_init, img_list))
 
 
